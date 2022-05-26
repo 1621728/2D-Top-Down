3e40fb35e6cae0643ab217664cbb9c6727989b8a
MUKILTEO\1621728 <1621728@mukilteo.wednet.edu>
2022-05-26 11:25:35 -0700

    .....

---
+++ 2D Top Down Ben/Assets/Scripts/sEEK.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sEEK : MonoBehaviour
+{
+    public Transform target;
+
+    private Rigidbody2D rb2;
+
+    public float thrustScale;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = new Vector2(target.transform.position.x - transform.position.x, target.transform.position.y - transform.position.y);
+        transform.up = direction;
+
+        //MoveForward
+        rb2.AddForce(transform.up * thrustScale * Time.deltaTime);
+    }
+}
